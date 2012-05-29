5f815eaccfe63cce7772cc1b1ebd2c00f3b0df1c
Kirill Krasnov <krasnovforum@gmail.com>
2012-05-29 13:54:28 +0300

    Some modification

--- JNS.php
+++ JNS.php
@@ -3,59 +3,81 @@ require_once( config_get_global( 'class_path' ) . 'MantisPlugin.class.php' );
 
 class JNSPlugin extends MantisPlugin {
 
-public function register()
-{
-// Название плагина
-$this->name = plugin_lang_get('title');
-// Описание плагина
-$this->description = plugin_lang_get('description');
-		$this->page        = 'config';
-// Версия плагина
-$this->version = '1.0';
-// Требования для плагина
-$this->requires = array(
-	'MantisCore' => '1.2.10',
-	);
-// Информация о разработчике
-$this->author = 'AcanthiS';
-$this->contact = 'acanthis@ya.ru';
-}
+    static $conn = null;
 
+    function register()
+    {
+        $this->name = plugin_lang_get( 'title' );
+        $this->description = plugin_lang_get( 'description' );
+        $this->page = 'config';
 
-public function hooks() {
-		return array(
-			'EVENT_BUGNOTE_ADD' => 'bugnote_add',
-		);
-	}
+        $this->version = '1.0';
+        $this->requires = array(
+            'MantisCore' => '1.2.10',
+        );
 
-function bugnote_add($bug_id, $bugnote_id) {
-	plugin_file_path("XMPPHP/XMPP.php");
+        $this->author = 'AcanthiS';
+        $this->contact = 'acanthis@ya.ru';
+    }
 
-	$sent_msg = "test";
-	$conn = new XMPPHP_XMPP('192.168.0.254' ,5222, 'info' ,0000 , 'xmpphp', 'nersisian' , $printlog=False, $loglevel='LOGGING_INFO');
-	$conn->connect();
-	$conn->processUntil('session_start');
-	$conn->message('acanthis@192.168.0.254', $sent_msg);
-	$conn->disconnect();
-	}
-    
+    function config() {
+        return array(
+            'xmpp_host'     => '127.0.0.1',
+            'xmpp_port'     => '5222',
+            'xmpp_user'     => 'support',
+            'xmpp_pass'     => '',
+            'xmpp_server'   => 'localhost',
+            'xmpp_printlog' => false,
+            'xmpp_loglevel' => 'none',
+        );
+    }
 
+    function init() {
+        require_once( 'XMPPHP/XMPP.php' );
 
-function schema() {
-		return array(
-			array( 'CreateTableSQL', array( plugin_table( 'user_table' ), "
-				id				I		NOTNULL UNSIGNED ZEROFILL AUTOINCREMENT PRIMARY,
-				user_id			I		NOTNULL UNSIGNED ZEROFILL ,
-				projects_id		T		NOTNULL,
-				jabber_login	T		NOTNULL,
-				jabber_pwd		T		NOTNULL,
-				jabber_server	T		NOTNULL				
-				" ) ),
-		);
-	}
+        $conn = new XMPPHP_XMPP(
+            plugin_config_get( 'xmpp_host' ),
+            plugin_config_get( 'xmpp_port' ),
+            plugin_config_get( 'xmpp_user' ),
+            plugin_config_get( 'xmpp_pass' ),
+            'mantisbt',
+            plugin_config_get( 'xmpp_server' ),
+            plugin_config_get( 'xmpp_printlog' ),
+            0
+        );
+    }
 
+    function hooks() {
+        return array(
+            'EVENT_BUGNOTE_ADD' => 'bugnote_add',
+        );
+    }
 
+    function bugnote_add($bug_id, $bugnote_id) {
 
+        $user_id = bug_get_field( $bug_id, 'reporter_id' );
+        $user = user_get_name( $user_id );
+
+        $msg = bugnote_get_text( $bugnote_id );
+
+        $conn->connect();
+        $conn->processUntil('session_start');
+        $conn->message($user . '@' . plugin_config_get( 'xmpp_server' ), $msg);
+        $conn->disconnect();
+    }
+
+//    function schema() {
+//            return array(
+//                array( 'CreateTableSQL', array( plugin_table( 'user_table' ), "
+//                    id				I		NOTNULL UNSIGNED ZEROFILL AUTOINCREMENT PRIMARY,
+//                    user_id			I		NOTNULL UNSIGNED ZEROFILL ,
+//                    projects_id		T		NOTNULL,
+//                    jabber_login	T		NOTNULL,
+//                    jabber_pwd		T		NOTNULL,
+//                    jabber_server	T		NOTNULL
+//                    " ) ),
+//            );
+//        }
 
 }
 ?>
