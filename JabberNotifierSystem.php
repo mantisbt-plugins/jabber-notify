e86daa5cc31ee1a2a19a57d6da06dc854cc448c4
Kirill Krasnov <krasnovforum@gmail.com>
2012-05-29 09:12:02 -0430

    rewrite code send message

--- JabberNotifierSystem.php
+++ JabberNotifierSystem.php
@@ -2,6 +2,7 @@
 require_once( config_get_global( 'class_path' ) . 'MantisPlugin.class.php' );
 
 class JabberNotifierSystemPlugin extends MantisPlugin {
+  var $conn;
 
   function register() {
     $this->name        = plugin_lang_get( 'title' );
@@ -40,7 +41,13 @@ class JabberNotifierSystemPlugin extends MantisPlugin {
    * Load the XMPP Class.
    */
   function init() {
-      require_once ( 'XMPPHP/XMPP.php' );
+    require_once ( plugin_file( './XMPPHP/XMPP.php' ) );
+    $jbr_server =  plugin_config_get( 'jbr_server' );
+    $jbr_port   =  plugin_config_get( 'jbr_port' );
+    $jbr_login  =  plugin_config_get( 'jbr_login' );
+    $jbr_pwd    =  plugin_config_get( 'jbr_pwd' );
+
+    $this->conn = new XMPPHP_XMPP($jbr_server, $jbr_port, $jbr_login, $jbr_pwd, 'mantisbt', $jbr_server , $printlog=False, $loglevel='LOGGING_INFO');
   }
 
   /**
@@ -59,42 +66,31 @@ class JabberNotifierSystemPlugin extends MantisPlugin {
     );
   }
 
-  function bugnote_add($p_event, $p_bug_id, $p_bugnote_id) {
+  function bugnote_add($p_bug_id, $p_bugnote_id) {
     global $g_path;
-    $t_user_table = db_get_table( 'mantis_user_table' );
-    $query = "SELECT username
-              FROM $t_user_table
-              WHERE id=". bug_get_field($p_bug_id, 'reporter_id');
-    $res = mysql_query($query);
 
-    while($row = mysql_fetch_array($res)) {
-      $juser = $row['username'];
-    }
+    $user_id = bug_get_field( $p_bug_id, 'reporter_id' );
+    $user_realname = user_get_realname( $user_id );
+    $user_name = user_get_name( $user_id );
 
-    $sent_msg = plugin_lang_get('bugnote_add_str1') . ' ' . user_get_realname(bug_get_field($p_bug_id, 'reporter_id')) . '! ' . plugin_lang_get('bugnote_add_str2') . "\n" .
+    $sent_msg = plugin_lang_get('bugnote_add_str1') . ' ' . $user_realname . '! ' . plugin_lang_get( 'bugnote_add_str2' ) . "\n" .
                 plugin_lang_get('bugnote_add_str3') . ' ' . bug_format_id( $p_bug_id, 'category_id' ) . "\n" .
-                plugin_lang_get('bugnote_add_str4') . ' ' . project_get_name(bug_get_field( $p_bug_id, 'project_id' )) . "\n" .
-                plugin_lang_get('bugnote_add_str5') . ' ' . get_enum_element( 'status',(bug_get_field( $p_bug_id, 'status' ))) . "\n" .
+                plugin_lang_get('bugnote_add_str4') . ' ' . project_get_name( bug_get_field( $p_bug_id, 'project_id' ) ) . "\n" .
+                plugin_lang_get('bugnote_add_str5') . ' ' . get_enum_element( 'status', ( bug_get_field( $p_bug_id, 'status' ) ) ) . "\n" .
                 plugin_lang_get('bugnote_add_str6') . ' ' . bug_get_field( $p_bug_id, 'summary' ) . "\n" .
                 plugin_lang_get('separator') . "\n" .
-                plugin_lang_get('bugnote_add_str7') . ' ' . user_get_realname($p_user_id ) . "\n" .
-                plugin_lang_get('bugnote_add_str8') . ' ' . bugnote_get_text($p_bugnote_id) . "\n" .
+                plugin_lang_get('bugnote_add_str7') . ' ' . $user_realname . "\n" .
+                plugin_lang_get('bugnote_add_str8') . ' ' . bugnote_get_text( $p_bugnote_id ) . "\n" .
                 plugin_lang_get('separator') . "\n" .
-                plugin_lang_get('bugnote_add_str9') . ' ' . $g_path .'view.php?id=' . $p_bug_id . "#c" . $p_bugnote_id;
+                plugin_lang_get('bugnote_add_str9') . ' ' . string_get_bugnote_view_url_with_fqdn( $p_bug_id, $p_bugnote_id );
 
-    $jbr_server =  plugin_config_get( 'jbr_server' );
-    $jbr_port   =  plugin_config_get( 'jbr_port' );
-    $jbr_login  =  plugin_config_get( 'jbr_login' );
-    $jbr_pwd    =  plugin_config_get( 'jbr_pwd' );
-
-    $conn = new XMPPHP_XMPP($jbr_server, $jbr_port, $jbr_login, $jbr_pwd, 'xmpphp', 'acanthis' , $printlog=False, $loglevel='LOGGING_INFO');
     //$conn->useEncryption(false);        //Enable this line if you get a error �Fatal error: Cannot access protected property XMPPHP_XMPP::$use_encryption� �
     try
     {
-      $conn->connect();
-      $conn->processUntil('session_start');
-      $conn->message($juser .'@'. $jbr_server, $sent_msg);
-      $conn->disconnect();
+      $this->conn->connect();
+      $this->conn->processUntil('session_start');
+      $this->conn->message($user_name .'@'. plugin_config_get( 'jbr_server' ), $sent_msg);
+      $this->conn->disconnect();
     }
     catch(XMPPHP_Exception $e)
     {
